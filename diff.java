a3ad7eb19f226bbd930a2811151da522b9ee8229
Roshan <rathodroshan1712@gmail.com>
2022-10-15 21:31:07 +0530

    aded diff.java with change

--- diff.java
+++ diff.java
@@ -1,4 +1,11 @@
 public class diff {
+    public static void main(String args []){
+        int a = 5;
+        int b=7;
+        int c= (a+b)/2;
+        System.out.println(c);
+
+    }
     
     
 }
